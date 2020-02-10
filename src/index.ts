6c6be2410ec69cd5c0b098f2ffae6b5ef21a4bfc
KonradM <miekus.konrad@gmail.com>
2020-02-10 08:09:41 +0100

    add fetch to models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { User } from './models/User'
+import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
 
-const user = User.buildUser({ id: 1 })
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+)
 
-user.on('change', () => console.log(user))
-user.fetch()
+collection.on('change', () => console.log(collection))
 
+collection.fetch()
