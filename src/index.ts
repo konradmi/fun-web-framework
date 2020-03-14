2379102c6b3914c806bcfbf55d85963b71106d26
KonradM <miekus.konrad@gmail.com>
2020-03-14 18:47:43 +0100

    Add collectionView

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { UserEdit } from './views/UserEdit'
-import { User } from './models/User'
+import { UserList } from './views/UserList'
+import { Collection } from './models/Collection'
+import { UserProps, User } from './models/User'
 
-const root = document.getElementById('root')
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+)
 
-if (root) {
-  const userEdit  = new UserEdit(root, User.buildUser({
-    name: 'NAME',
-    age: 20
-  }))
-  userEdit.render()
-} else {
-  throw new Error('Root element not found')
-}
+users.on('change', () => {
+  const root = document.getElementById('root')
+
+  if (root) {
+    new UserList(root, users).render()
+  }
+})
+
+users.fetch()
